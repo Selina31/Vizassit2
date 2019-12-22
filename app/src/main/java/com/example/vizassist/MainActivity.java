c65768bb7968a2f306ae8799df3fbc4ae30962d2
Selina Du <acelinvn@gmail.com>
2019-12-21 19:08:17 -0600

    First

---
+++ app/src/main/java/com/example/vizassist/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.vizassist;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.vizassist.imagepipeline.ImageActions;
+import com.example.vizassist.utilities.HttpUtilities;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.sql.Time;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String UPLOAD_HTTP_URL = "http://34.67.193.198:8080/vizassist/annotate";
+
+    private static final int IMAGE_CAPTURE_CODE = 1;
+    private static final int SELECT_IMAGE_CODE = 2;
+
+
+    private static final int CAMERA_PERMISSION_REQUEST = 1001;
+
+    private MainActivityUIController mainActivityUIController;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainActivityUIController = new MainActivityUIController(this);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mainActivityUIController.resume();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_capture:
+                mainActivityUIController.updateResultView(getString(R.string.result_placeholder));
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    mainActivityUIController.askForPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_REQUEST);
+
+                }else{
+                    ImageActions.startCameraActivity(this, IMAGE_CAPTURE_CODE);
+                }
+                break;
+            case R.id.action_gallery:
+                mainActivityUIController.updateResultView(getString(R.string.result_placeholder));
+                ImageActions.startCameraActivity(this, SELECT_IMAGE_CODE);
+                break;
+            default:
+                    break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            Bitmap bitmap = null;
+            if(requestCode == IMAGE_CAPTURE_CODE){
+                bitmap = (Bitmap)data.getExtras().get("data");
+                mainActivityUIController.updateImageViewWithBitmap(bitmap);
+            }else if(requestCode == SELECT_IMAGE_CODE){
+                Uri selectedImage = data.getData();
+                try{
+                    bitmap = MediaStore.Images.Media.getContentUri(this.getContentResolver(), selectedImage);
+                    mainActivityUIController.updateImageViewWithBitmap(bitmap);
+                }catch (IOException e){
+                    mainActivityUIController.showErrorDialogWithMessage(R.string.reading_error_message);
+                }
+            }
+        }
+    }
+
+    private void uploadImage(Bitmap bitmap) {
+    }
+}
---
+++ app/src/main/java/com/example/vizassist/MainActivityUIController.java
@@ -0,0 +1,59 @@
+package com.example.vizassist;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Controller of main activity.
+ */
+public class MainActivityUIController {
+    private final Activity activity;
+    private final Handler mainThreadHandler;
+
+    private TextView resultView;
+    private ImageView imageView;
+
+    public MainActivityUIController(Activity activity) {
+        this.activity = activity;
+        this.mainThreadHandler = new Handler(Looper.getMainLooper());
+    }
+
+    public void resume() {
+        resultView = activity.findViewById((R.id.resultView));
+        imageView = activity.findViewById((R.id.capturedImage));
+    }
+
+    public void updateResultView(final String text) {
+    }
+
+    public void updateImageViewWithBitmap(Bitmap bitmap) {
+        imageView.setImageBitmap(bitmap);
+    }
+
+    public void showErrorDialogWithMessage(int messageStringID) {
+    }
+
+    public void showInternetError() {
+    }
+
+    public void askForPermission(String permission, Integer requestCode) {
+        if(ContextCompat.checkSelfPermission(activity,permission) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(activity,permission)){
+                ActivityCompat.requestPermissions(activity,new String[]{permission},requestCode);
+            }else{
+                ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+            }
+        }
+    }
+}
